2ccc6b4a1fc81bb4bb908c994b10733886a2b585
Ian Cohen <ian_cohen90@live.com>
2021-07-22 20:23:14 -0400

    started save button function and storing text in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,21 @@
 var currentDate = moment().format('[Today is] dddd, MMMM Do, YYYY');
 $('#currentDay').html(currentDate);
 
+$(document).ready(function(){
+    $(".saveBtn").on("click", function (){
+        
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
+        localStorage.setItem(time, text);
+    })
+
+
+    $("#h9 .description").val(localStorage.getItem("h9"));
+
+
+
+})
 
 
 
